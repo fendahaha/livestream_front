d7342d9fbaad0ed0aa43dbc80d569af50afbd466
fenda <troneyf001@gmail.com>
2023-12-05 01:58:23 +0800

    asd

--- src/app/(front)/test/Stomp.js
+++ src/app/(front)/test/Stomp.js
@@ -1,57 +1,101 @@
 'use client'
 import {Client} from '@stomp/stompjs';
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useMemo, useReducer, useRef, useState} from "react";
 import {Button, Input, Space} from "antd";
 
-export default function Stomp() {
+const destination = '/topic/a';
+const msg = 'hello';
 
+function reducer(state, action) {
+    if (action) {
+        return [...state, action]
+    }
+}
+
+export default function Stomp() {
+    const [state, dispatch] = useReducer(reducer, [], r => r);
+    const clientRef = useRef(null);
+    const onSend = () => {
+        if (clientRef.current && clientRef.current.connected) {
+            clientRef.current.publish({destination: destination, body: msg, headers: {priority: '9'}});
+        }
+    };
+    const onActivete = () => {
+        if (clientRef.current) {
+            clientRef.current.activate()
+        }
+    }
+    const onDeactivete = () => {
+        if (clientRef.current) {
+            clientRef.current.deactivate()
+        }
+    }
+    const subscribe = () => {
+        if (clientRef.current && clientRef.current.connected) {
+            clientRef.current.subscribe(destination, (message) => {
+                let m = '';
+                let d = new Date().toLocaleTimeString();
+                if (message.body) {
+                    m = d + 'got message with body ' + message.body
+                } else {
+                    m = d + 'got empty message';
+                }
+                dispatch(m)
+            });
+        }
+    }
+    const unsubscribe = () => {
+        if (clientRef.current && clientRef.current.connected) {
+            clientRef.current.unsubscribe()
+        }
+    }
     useEffect(() => {
         // console.log(Client);
-        let destination = '/topic/a';
-        let msg = 'hello';
-        let client = new Client({
-            brokerURL: 'ws://localhost:8080/portfolio?token=df12sda&username=fenda1',
-            connectHeaders: {
-                login: 'user',
-                passcode: 'password',
-                username: 'fenda1',
-            },
-            debug: function (str) {
-                console.log("debug:", str);
-            },
-            reconnectDelay: 5000,
-            heartbeatIncoming: 4000,
-            heartbeatOutgoing: 4000,
-        });
-        client.onConnect = function (frame) {
-            console.log(JSON.stringify(frame));
+        if (!clientRef.current) {
+            clientRef.current = new Client({
+                brokerURL: 'ws://localhost:8080/portfolio?token=df12sda&username=fenda1',
+                connectHeaders: {
+                    login: 'user',
+                    passcode: 'password',
+                    username: 'fenda1',
+                },
+                debug: function (str) {
+                    console.log("debug:", str);
+                },
+                onConnect: function (frame) {
+                    console.log(JSON.stringify(frame));
 
-        };
-
-        setTimeout(() => {
-            client.activate();
-            client.subscribe(destination, (message) => {
-                if (message.body) {
-                    alert('got message with body ' + message.body);
-                } else {
-                    alert('got empty message');
-                }
+                },
+                // reconnectDelay: 5000,
+                // heartbeatIncoming: 4000,
+                // heartbeatOutgoing: 4000,
             });
-            client.publish({destination: destination, body: msg, headers: {priority: '9'}});
-        }, 1000 * 2)
+        }
 
-        // return () => {
-        //     client.deactivate();
-        // }
+        return () => {
+            if (clientRef.current.connected) {
+                clientRef.current.deactivate();
+            }
+        }
     }, []);
     return (
         <div>
-            <div className={'d1'}></div>
+            <div className={'d1'}>
+                {state.map(e => <div key={Math.random() + (new Date()).toLocaleTimeString()}>{e}</div>)}
+            </div>
+            <Space>
+                <Button onClick={onActivete}>activete</Button>
+                <Button onClick={onDeactivete}>deactivete</Button>
+                <Button onClick={subscribe}>subscribe</Button>
+                <Button onClick={unsubscribe}>unsubscribe</Button>
+                <Button onClick={onSend}>send</Button>
+            </Space>
             <style jsx>{`
               .d1 {
                 width: 100%;
                 height: 300px;
                 border: 1px solid red;
+                overflow: auto;
               }
             `}</style>
         </div>
