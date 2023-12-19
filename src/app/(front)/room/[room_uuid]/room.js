0f981148bd646c72689482f097c3f194b66417e9
fendahaha <troneyf001@gmail.com>
2023-12-19 20:27:07 +0800

    FlvContainer

--- src/app/(front)/room/[room_uuid]/room.js
+++ src/app/(front)/room/[room_uuid]/room.js
@@ -3,7 +3,7 @@ import styles from './room.module.css';
 import FlvContainer from "@/component/player/flv_container";
 import ChatSendButton from "@/component/chat/ChatSendButton";
 import {ChatMsgs} from "@/component/chat/chatMsgs";
-import {useCallback, useEffect, useReducer, useRef, useState} from "react";
+import {useCallback, useContext, useEffect, useReducer, useRef, useState} from "react";
 import {Client} from "@stomp/stompjs";
 import {FendaDanmu} from "@/app/(front)/room/[room_uuid]/BarrageMessages";
 import {MyTabs} from "@/app/(front)/room/[room_uuid]/tabs";
@@ -12,6 +12,8 @@ import {v4} from "uuid";
 import {clientBackendFetch, streamServer, wsPrefix} from "@/util/requestUtil";
 import Gifts from "@/app/(front)/room/[room_uuid]/Gifts";
 import {FendaGifts} from "@/app/(front)/room/[room_uuid]/GiftMessages";
+import {GlobalContext} from "@/app/(front)/component/globalContext";
+import {message} from "antd";
 
 
 const MessageUtil = {
@@ -47,7 +49,7 @@ const MessageUtil = {
     }
 }
 
-const useStomp = (destinationTopic) => {
+const useStomp = (destinationTopic, user, anchor) => {
     const [chatMessages, dispatchChatMessages] = useReducer(
         (state, action) => {
             let _new = [action, ...state];
@@ -85,16 +87,20 @@ const useStomp = (destinationTopic) => {
     const danmuRef = useRef(null);
     const giftRef = useRef(null);
     const sendMessage = useCallback((msg) => {
-        if (stompClientRef.current && stompClientRef.current.connected) {
-            let headers = {priority: '9'}
-            stompClientRef.current.publish({
-                destination: destinationTopic,
-                body: JSON.stringify(msg),
-                headers: headers
-            });
-            return true
+        if (user) {
+            if (stompClientRef.current && stompClientRef.current.connected) {
+                let headers = {anchorUuid: anchor.anchorUuid, anchorName: anchor.user.userName}
+                stompClientRef.current.publish({
+                    destination: destinationTopic,
+                    body: JSON.stringify(msg),
+                    headers: headers
+                });
+                return true
+            }
+        } else {
+            message.info("please log in");
         }
-    }, [destinationTopic]);
+    }, [destinationTopic, user, anchor]);
     const sendChatMessage = useCallback((msg) => sendMessage(MessageUtil.createChatMessage(msg)), [sendMessage]);
     const sendGiftMessage = useCallback((msg) => sendMessage(MessageUtil.createGiftMessage(msg)), [sendMessage]);
     const sendSystemMessage = useCallback((msg) => sendMessage(MessageUtil.createSystemMessage(msg)), [sendMessage]);
@@ -102,15 +108,14 @@ const useStomp = (destinationTopic) => {
     useEffect(() => {
         if (!stompClientRef.current) {
             stompClientRef.current = new Client({
-                brokerURL: `${wsPrefix}?token=df12sda&username=fenda1`,
+                brokerURL: `${wsPrefix}`,
                 connectHeaders: {
-                    login: 'user',
-                    passcode: 'password',
-                    username: 'fenda13',
+                    // passcode: 'password',
+                    user: user ? JSON.stringify(user) : null,
                 },
                 connectionTimeout: 10 * 1000,
                 debug: function (str) {
-                    // console.log("debug:", str);
+                    console.log("debug:", str);
                 },
                 onStompError: function (frame) {
                     // Will be invoked in case of error encountered at Broker
@@ -119,9 +124,6 @@ const useStomp = (destinationTopic) => {
                     // Compliant brokers will terminate the connection after any error
                     console.log('Broker reported error: ' + frame.headers['message']);
                     console.log('Additional details: ' + frame.body);
-                },
-                onWebSocketClose: (evt) => {
-                    console.log("onWebSocketClose: 远程主机连接断开");
                 },
                 reconnectDelay: 5000,
                 // heartbeatIncoming: 4000,
@@ -156,17 +158,20 @@ const useStomp = (destinationTopic) => {
                     }
                 });
             }
-        }
-        if (stompClientRef.current && !stompClientRef.current.connected) {
-            stompClientRef.current.activate();
+            stompClientRef.current.onWebSocketClose = function (evt) {
+                console.log("onWebSocketClose: 远程主机连接断开");
+            }
+            if (stompClientRef.current && !stompClientRef.current.connected) {
+                stompClientRef.current.activate();
+            }
         }
         return () => {
-            if (stompClientRef.current && stompClientRef.current.connected) {
+            if (stompClientRef.current) {
                 stompClientRef.current.deactivate();
                 stompClientRef.current = null;
             }
         }
-    }, [destinationTopic]);
+    }, [destinationTopic, user]);
     return [danmuRef, giftRef, chatMessages, giftMessages, systemMessages, sendChatMessage, sendGiftMessage, sendSystemMessage]
 }
 
@@ -179,12 +184,14 @@ const query = (room_uuid) => {
 }
 
 export default function Room({uuid}) {
+    const {user} = useContext(GlobalContext);
     const [anchor, setAnchor] = useState(null);
     const [streamUrl, setStreamUrl] = useState(null);
     useEffect(() => {
         if (!anchor) {
             query(uuid).then(anchor => {
                 setAnchor(anchor);
+                console.log(anchor);
                 if (anchor?.room?.streamAddress) {
                     let s = `${streamServer}${anchor?.room?.streamAddress}.flv?${anchor.room.streamParam}`;
                     setStreamUrl(s);
@@ -192,7 +199,7 @@ export default function Room({uuid}) {
             })
         }
     }, [anchor, uuid]);
-    const [danmuRef, giftRef, chatMessages, giftMessages, systemMessages, sendChatMessage, sendGiftMessage, sendSystemMessage] = useStomp(`/topic/${uuid}`);
+    const [danmuRef, giftRef, chatMessages, giftMessages, systemMessages, sendChatMessage, sendGiftMessage, sendSystemMessage] = useStomp(`/topic/${uuid}`, user, anchor);
     return (
         <div className={styles.container}>
             <div className={styles.layout1}>
