7d8a925649ff5f9eb42c1427a4c1cc18c882ccee
fendahaha <troneyf001@gmail.com>
2023-12-27 16:13:42 +0800

    asd

--- src/app/(front)/user/anchor/tab2.js
+++ src/app/(front)/user/anchor/tab2.js
@@ -1,9 +1,12 @@
-import React, {useCallback, useRef} from 'react';
+import React, {useCallback, useMemo, useRef, useState} from 'react';
 import {ProDescriptions} from "@ant-design/pro-components";
-import {clientBackendFetch, rtmpServer} from "@/util/requestUtil";
-import {message} from "antd";
+import {clientBackendFetch, imagePrefix, rtmpServer} from "@/util/requestUtil";
+import {message, Typography, Upload} from "antd";
+import styles from './tab2.module.css';
+import {DeleteOutlined, InboxOutlined} from "@ant-design/icons";
 
-const App = ({anchor}) => {
+const AnchorInfo = ({anchor}) => {
+    const {user, room} = anchor;
     const actionRef = useRef();
     const columns = [
         {
@@ -12,6 +15,7 @@ const App = ({anchor}) => {
             copyable: true,
             editable: false,
             ellipsis: true,
+            span: 3,
         },
         {
             title: '是否可以直播',
@@ -26,12 +30,14 @@ const App = ({anchor}) => {
             title: '直播类型',
             dataIndex: 'streamType',
             editable: false,
+            span: 2,
         },
         {
             title: '直播房间标题',
             dataIndex: 'anchorRemark',
             copyable: true,
             ellipsis: true,
+            span: 3,
         },
         {
             title: '三维',
@@ -69,14 +75,14 @@ const App = ({anchor}) => {
             })
     }, [anchor])
     const handleRequest = useCallback(() => {
-        const user = anchor.user;
-        const room = anchor.room;
         let s = {};
         if (anchor.anchorConfig) {
             s = JSON.parse(anchor.anchorConfig)
         }
         let d = {
-            ...s,
+            anchorSanwei: s.anchorSanwei,
+            anchorHeight: s.anchorHeight,
+            anchorWieght: s.anchorWieght,
             roomEnable: room.roomEnable,
             streamAddress: `${rtmpServer}/${room.streamAddress}`,
             streamType: room.streamType,
@@ -87,7 +93,7 @@ const App = ({anchor}) => {
     return (
         <div>
             <ProDescriptions
-                column={1}
+                column={3}
                 actionRef={actionRef}
                 title={''}
                 request={handleRequest}
@@ -98,4 +104,138 @@ const App = ({anchor}) => {
         </div>
     );
 };
-export default App;
+
+function get_attribute_of_anchorConfig(anchorConfig, attributeName, defaultValue) {
+    let config = {};
+    if (anchorConfig) {
+        config = JSON.parse(anchorConfig)
+    }
+    return config[attributeName] ? config[attributeName] : defaultValue;
+}
+
+function update_attribute_of_anchorConfig(anchorConfig, attributeName, defaultValue, callback) {
+    let config = {};
+    if (anchorConfig) {
+        config = JSON.parse(anchorConfig)
+    }
+    let v = config[attributeName] ? config[attributeName] : defaultValue;
+    config[attributeName] = callback(v)
+    return JSON.stringify(config);
+}
+
+const AnchorImage = ({filePath, del}) => {
+    return (
+        <div className={styles.anchor_image}>
+            <img src={`${imagePrefix}/${filePath}`} alt={''} className={styles.anchor_image_item}/>
+            <div className={styles.anchor_image_del}>
+                <DeleteOutlined onClick={() => del(filePath)}/>
+            </div>
+        </div>
+    )
+}
+
+const AnchorImages = ({anchor}) => {
+    const anchorUuid = anchor.anchorUuid;
+    const [anchorConfig, setAnchorConfig] = useState(anchor.anchorConfig)
+    const upload_success_callback = (fileObject) => {
+        let new_anchorConfig = update_attribute_of_anchorConfig(anchorConfig, 'covers', [], (covers) => {
+            return [...covers, fileObject.filePath]
+        });
+        clientBackendFetch.postJson("/anchor/update2", {anchorUuid, anchorConfig: new_anchorConfig})
+            .then(r => {
+                if (r?.data) {
+                    setAnchorConfig(new_anchorConfig);
+                    message.success("上传成功");
+                }
+            })
+            .catch(() => {
+                message.success("上传失败");
+            })
+    }
+    const del = (filePath) => {
+        let new_anchorConfig = update_attribute_of_anchorConfig(anchorConfig, 'covers', [], (covers) => {
+            let index = covers.indexOf(filePath)
+            if (index > -1) {
+                covers.splice(index, 1)
+            }
+            return [...covers]
+        });
+        clientBackendFetch.postJson("/anchor/update2", {anchorUuid, anchorConfig: new_anchorConfig})
+            .then(r => {
+                if (r?.data) {
+                    setAnchorConfig(new_anchorConfig);
+                    message.success("success");
+                }
+            })
+            .catch(() => {
+                message.success("fail");
+            })
+    }
+    const images = useMemo(() => {
+        let covers = get_attribute_of_anchorConfig(anchorConfig, 'covers', []);
+        return covers.map(filePath => <AnchorImage key={filePath} filePath={filePath} del={del}/>)
+    }, [anchorConfig, del]);
+    const props = {
+        accept: 'image/*',
+        action: "/backend/file/upload",
+        name: "file",
+        data: {category: 'anchor_avatar'},
+        withCredentials: true,
+        showUploadList: false,
+        multiple: false,
+        onDrop(e) {
+            console.log('Dropped files', e.dataTransfer.files);
+        },
+        onChange(info) {
+            const {file} = info;
+            if (file.status === 'uploading') {
+                return;
+            }
+            if (file.status === 'done') {
+                const {name, response} = file
+                if (response.data[name]) {
+                    upload_success_callback(response.data[name]);
+                } else {
+                    message.error("上传失败")
+                }
+            }
+            if (file.status === 'error') {
+                console.log(info);
+                message.error("上传失败（网络错误）")
+            }
+        },
+    };
+    return (
+        <>
+            <div className={styles.anchor_images}>
+                {/*{data.map(filePath => <AnchorImage key={filePath} data={filePath}/>)}*/}
+                {images}
+                <div>
+                    <Upload.Dragger {...props} className={styles.fd_ant_upload}>
+                        <p className="ant-upload-drag-icon">
+                            <InboxOutlined/>
+                        </p>
+                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                        <p className="ant-upload-hint">
+                            Support for a single or bulk upload. Strictly prohibited from uploading company data or
+                            other
+                            banned files.
+                        </p>
+                    </Upload.Dragger>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default function Page({anchor}) {
+    return (
+        <>
+            <AnchorInfo anchor={anchor}/>
+            <Typography.Title ellipsis={true} level={2} type={'secondary'} style={{textAlign: "center"}}>
+                主播封面图
+            </Typography.Title>
+            <AnchorImages anchor={anchor}/>
+        </>
+    );
+}
