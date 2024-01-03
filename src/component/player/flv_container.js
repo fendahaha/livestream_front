dfdfe2f235372812f538904c448eca0c5c331622
fendahaha <troneyf001@gmail.com>
2024-01-03 21:57:35 +0800

    asd

--- src/component/player/flv_container.js
+++ src/component/player/flv_container.js
@@ -1,56 +1,84 @@
 'use client'
 import {useEffect, useRef, useState} from "react";
-import {message} from "antd";
+import {message, Spin} from "antd";
 import {useMyLocale} from "@/component/context/localeContext";
 import Script from "next/script";
+import styles from './flv_container.module.css';
+import {LoadingOutlined} from "@ant-design/icons";
+
+const MyLoading = ({isLoading = true}) => {
+    return (
+        <>
+            {isLoading ?
+                <div className={styles.loading}>
+                    <LoadingOutlined/>
+                </div>
+                : ''
+            }
+        </>
+    )
+}
 
 export default function FlvContainer({url}) {
     const {getDict} = useMyLocale('Room');
+    const [flv, setFlv] = useState(null);
+    const [flvLoading, setFlvLoading] = useState(true);
     const videoRef = useRef(null);
     const flvPlayerRef = useRef(null);
-    const [isStreamOnline, setIsStreamOnline] = useState(true);
+    const [shouldReplay, setShouldReplay] = useState(false);
     useEffect(() => {
-        if (flvjs.isSupported()) {
-            const flvPlayer = flvjs.createPlayer({
-                type: 'flv',
-                url: url,
-                isLive: true,
-                enableStashBuffer: false,
-                // withCredentials: true,
-                cors: true,
-            });
-            flvPlayer.attachMediaElement(videoRef.current);
-            flvPlayer.on(flvjs.Events.ERROR, (errorType, errorInfo) => {
-                console.log(errorType, errorInfo);
-                message.warning(getDict('anchor_offline'), 5);
-                setIsStreamOnline(false);
-            });
-            flvPlayerRef.current = flvPlayer;
-            flvPlayer.load();
-            flvPlayer.play();
+        if (flv) {
+            if (flv.isSupported()) {
+                const flvPlayer = flv.createPlayer({
+                    type: 'flv',
+                    url: url,
+                    isLive: true,
+                    enableStashBuffer: false,
+                    // withCredentials: true,
+                    cors: true,
+                });
+                flvPlayer.attachMediaElement(videoRef.current);
+                flvPlayer.on(flv.Events.ERROR, (errorType, errorInfo) => {
+                    console.log(errorType, errorInfo);
+                    message.warning(getDict('anchor_offline'), 5);
+                    setShouldReplay(true);
+                });
+                flvPlayer.on(flv.Events.MEDIA_INFO,(e)=>{
+                    console.log(e);
+                })
+                flvPlayerRef.current = flvPlayer;
+                flvPlayer.load();
+                flvPlayer.play();
 
-            return () => {
-                flvPlayer.destroy();
-            };
-        } else {
-            message.info('flv is not supported');
+                return () => {
+                    flvPlayer.destroy();
+                };
+            } else {
+                message.info('flv is not supported');
+            }
         }
-    }, [url]);
+    }, [flv, url]);
     useEffect(() => {
-        if (!isStreamOnline) {
+        if (shouldReplay) {
+            setShouldReplay(false);
             flvPlayerRef.current.unload();
             flvPlayerRef.current.load();
             flvPlayerRef.current.play();
-            setIsStreamOnline(true);
         }
-    }, [isStreamOnline]);
+    }, [shouldReplay]);
     return (
-        <div style={{
-            width: '100%', height: '100%', position: 'relative',
-        }}>
+        <>
             <Script src={'https://cdnjs.cloudflare.com/ajax/libs/flv.js/1.6.2/flv.min.js'}
-                    strategy={'beforeInteractive'}/>
-            <video ref={videoRef} controls autoPlay muted style={{width: '100%', height: '100%',}}/>
-        </div>
+                    strategy={'afterInteractive'}
+                    onReady={() => {
+                        setFlv(flvjs);
+                        setFlvLoading(false);
+                    }}
+            />
+            <div className={styles.video_container}>
+                <MyLoading isLoading={flvLoading}/>
+                <video ref={videoRef} controls autoPlay muted className={styles.video}/>
+            </div>
+        </>
     )
 }
