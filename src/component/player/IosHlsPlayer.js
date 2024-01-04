3100aa529794f001cd3cd79431ff265c7c776bbe
fendahaha <troneyf001@gmail.com>
2024-01-04 15:32:39 +0800

    asd

--- src/component/player/IosHlsPlayer.js
+++ src/component/player/IosHlsPlayer.js
@@ -5,8 +5,13 @@ export default function IosHlsPlayer({url, param}) {
     const videoRef = useRef(null);
     return (
         <div style={{width: '100%', height: '100%', position: 'relative'}}>
-            <video ref={videoRef} controls autoPlay muted playsInline style={{width: '100%', height: '100%',}}
-                   src={streamUrl}/>
+            <video controls autoPlay muted playsInline style={{width: '100%', height: '100%',}}
+                   ref={videoRef}
+                   src={streamUrl}
+                   onError={(e) => {
+                        console.log(e);
+                   }}
+            />
         </div>
     );
 }
