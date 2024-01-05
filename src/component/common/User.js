369fec44843319515a3d8ecf628e06e4fbc0ebb1
fendahaha <troneyf001@gmail.com>
2024-01-05 17:00:36 +0800

    asd

--- src/component/common/User.js
+++ src/component/common/User.js
@@ -77,6 +77,7 @@ export default function User() {
                                     <Button danger block={true} onClick={() => {
                                         logout(() => {
                                             updateUser({action: 'replace', data: null});
+                                            set_user(null);
                                             router.refresh();
                                             message.success(getDict('logoutSuccessMsg'));
                                         });
