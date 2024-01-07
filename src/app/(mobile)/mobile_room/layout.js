0ddca346148408bcb5ccefe5624baf02335a60f2
fendahaha <troneyf001@gmail.com>
2024-01-07 14:03:13 +0800

    asd

--- src/app/(mobile)/mobile_room/layout.js
+++ src/app/(mobile)/mobile_room/layout.js
@@ -1,6 +1,6 @@
 export default function Component({children}) {
     return (
-        <div style={{width: '100vw', height: '100vh'}}>
+        <div style={{width: '100%', height: '100%'}}>
             {children}
         </div>
     );
